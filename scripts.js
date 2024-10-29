1017b4e9bd29dd449d321f6b9d1250267010d29b
AstronGaming <yashudkl@gmail.com>
2024-10-29 21:29:44 +0545

    initial-slider-on-page-load

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-//An array defined for three image sliders
+// An array defined for three image sliders
 const images = [
   "./img/slider-3.jpeg",
   "./img/slider-2.jpeg",
@@ -6,24 +6,44 @@ const images = [
 ];
 
 let currentIndex = 0;
+
+// Preloads images and initialize the slider once the page is loaded for user
+function preloadImages(imageArray, callback) {
+  let loadedImages = 0;
+  const totalImages = imageArray.length;
+
+  imageArray.forEach((src) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => {
+      loadedImages++;
+      if (loadedImages === totalImages) {
+        callback();
+      }
+    };
+  });
+}
+
 // Function which shows image index from images array
 function showImage(index) {
   currentIndex = index;
   document.getElementById('slider-image').src = images[currentIndex];
   updateIndicators();
 }
- //Function which leads user to next slider image
+
+// Function which leads user to next slider image
 function nextImage() {
   currentIndex = (currentIndex + 1) % images.length;
   showImage(currentIndex);
 }
- //Function which leads user to previous slider image
+
+// Function which leads user to previous slider image
 function prevImage() {
   currentIndex = (currentIndex - 1 + images.length) % images.length;
   showImage(currentIndex);
 }
 
-//Updates dot incicators
+// Updates dot indicators
 function updateIndicators() {
   const dots = document.querySelectorAll('.dot');
   dots.forEach((dot, index) => {
@@ -31,11 +51,12 @@ function updateIndicators() {
   });
 }
 
-// Automatic sliding every 2 seconds
-setInterval(nextImage, 2000);
-
-// Initial image display
-showImage(currentIndex);
+// Initial image display after reloading or preloading of the page
+preloadImages(images, () => {
+  showImage(currentIndex);
+  // Automatic sliding every 2 seconds
+  setInterval(nextImage, 2000);
+});
 
 const lists = document.querySelectorAll('.list');
 
@@ -49,4 +70,3 @@ lists.forEach((list) => {
     }, 300);
   });
 });
-
